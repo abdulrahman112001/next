859869d9e287d68ac955d22980c4295a30028822
abdulrahman112001 <abdulrahmanelshikh2001@gmail.com>
2023-06-05 16:09:20 +0200

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  const metadata = {
+    title: "ddd", 
+    description : "adddf"
+  }
+
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
---
+++ app/component/Navbar.tsx
@@ -0,0 +1,111 @@
+"use client"
+import {
+  createStyles,
+  Header,
+  Menu,
+  Group,
+  Center,
+  Burger,
+  Container,
+  rem,
+} from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { IconChevronDown } from "@tabler/icons-react"
+import { MantineLogo } from "@mantine/ds"
+import Link from "next/link"
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    backgroundColor: theme.fn.variant({
+      variant: "filled",
+      color: theme.primaryColor,
+    }).background,
+    borderBottom: 0,
+  },
+
+  inner: {
+    height: rem(56),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color: theme.white,
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor: theme.fn.lighten(
+        theme.fn.variant({ variant: "filled", color: theme.primaryColor })
+          .background!,
+        0.1
+      ),
+    },
+  },
+
+  linkLabel: {
+    marginRight: rem(5),
+  },
+}))
+
+interface HeaderSearchProps {
+  links?: {
+    link?: string
+    label?: string
+    links?: { link: string; label: string }[]
+  }[]
+}
+
+export function Navbar({ links }: HeaderSearchProps) {
+  const [opened, { toggle }] = useDisclosure(false)
+  const { classes } = useStyles()
+
+  const items = links?.map((link) => {
+    const menuItems = link?.links?.map((item) => (
+      <Menu.Item key={item?.link}>{item.label}</Menu.Item>
+    ))
+
+
+  })
+
+  return (
+    <Header height={56} className={classes.header} mb={120}>
+      <Container>
+        <div className={classes.inner}>
+          <MantineLogo size={28} inverted />
+          <Group spacing={20} className={classes.links}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/about">Contact us </Link>
+            <Link href="/about">News</Link>
+          </Group>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+            color="#fff"
+          />
+        </div>
+      </Container>
+    </Header>
+  )
+}
